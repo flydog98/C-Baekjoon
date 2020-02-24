1d326914940e780194428d599f0eba938100a1a6
flydog98 <pyjpyj0825@gmail.com>
2020-02-24 17:18:49 +0900

    ..

---
+++ C++10430+.cpp
@@ -0,0 +1,20 @@
+//10430번 나머지
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int a = 0, b = 0, c = 0;
+	int temp = 0;
+
+	cin >> a >> b >> c;
+
+	cout << (a + b) % c << endl;
+	cout << (a % c + b % c) % c << endl;
+	cout << (a * b) % c << endl;
+	cout << (a % c * (b % c)) % c << endl;
+
+	return 0;
+
+}
